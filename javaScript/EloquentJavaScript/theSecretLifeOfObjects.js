78c6504339102de3361e7e2748b49b59277b89cf
Ryan Cutter <ryancutter.mail@gmail.com>
2018-02-20 18:19:31 +0000

    Eloquent JavaScript: 🚧 🎓 Exercises added

---
+++ javaScript/EloquentJavaScript/theSecretLifeOfObjects.js
@@ -0,0 +1,32 @@
+//Program Structure Exercises
+console.log('--------------------------- The Secret Life of Objects Exercises ---------------------------');
+
+
+// .d888888     dP     dP                     dP                        d888888P                                   
+//d8'    88     88     88                     88                           88                                      
+//88aaaaa88a    88    .8P .d8888b. .d8888b. d8888P .d8888b. 88d888b.       88    dP    dP  88d888b. .d8888b.       
+//88     88     88    d8' 88ooood8 88'  `""   88   88'  `88 88'  `88       88    88    88  88'  `88 88ooood8       
+//88     88     88  .d8P  88.  ... 88.  ...   88   88.  .88 88             88    88.  .88  88.  .88 88.  ...       
+//88     88     888888'   `88888P' `88888P'   dP   `88888P' dP             dP    `8888P88  88Y888P' `88888P'       
+//oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo~~~~.88~~88~ooooooooooooooooooooo
+//                                                                                d8888P   dP                        
+console.log('\n---------------------------\n A Vector Type \n---------------------------');
+
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function (vectorIn) {
+    return new Vector(this.x + vectorIn.x, this.y + vectorIn.y);
+
+};
+Vector.prototype.minus = function (vectorIn) {
+    return new Vector(this.x - vectorIn.x, this.y - vectorIn.y);
+};
+
+//Add getter
+
+
+console.log(new Vector(2, 2).plus(new Vector(2, 2)));
+console.log(new Vector(4, 4).minus(new Vector(2, 2)));
