362fce8d6f142dd1154187198f117102b684cece
Ryan Cutter <ryancutter.mail@gmail.com>
2018-06-25 19:43:31 +0100

    hackernews-node: chapter 3

--- GraphQL/hackernews-node/src/index.js
+++ GraphQL/hackernews-node/src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
 let links = [
   {
     id: "link-0",
@@ -21,20 +8,28 @@ let links = [
   }
 ];
 
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links
   },
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
+
 server.start(() => console.log(`Server is running on http://localhost:4000`));
