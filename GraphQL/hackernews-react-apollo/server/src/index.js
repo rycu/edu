2d9a37169a7fd1094aae5bf0b6b71ad68c18b107
Ryan Cutter <ryancutter.mail@gmail.com>
2018-07-09 07:56:11 +0100

    FIX: hackernews-react-apollo: chapter 1

--- GraphQL/hackernews-react-apollo/server/src/index.js
+++ GraphQL/hackernews-react-apollo/server/src/index.js
@@ -1,29 +1,30 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { Prisma } = require('prisma-binding')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const Subscription = require('./resolvers/Subscription')
-const Feed = require('./resolvers/Feed')
+const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
+const Feed = require("./resolvers/Feed");
 
 const resolvers = {
   Query,
   Mutation,
   Subscription,
-  Feed,
-}
+  Feed
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: req => ({
     ...req,
     db: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://eu1.prisma.sh/public-charmpainter-619/hackernews-graphql-js/dev',
-      secret: 'mysecret123',
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint:
+        "https://eu1.prisma.sh/public-inkwatcher-330/hackernews-graphql-js/dev",
+      secret: "mysecret123",
       debug: true
-    }),
-  }),
-})
+    })
+  })
+});
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log("Server is running on http://localhost:4000"));
